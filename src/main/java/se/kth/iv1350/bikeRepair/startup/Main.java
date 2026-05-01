ca582b8d38b348a3d90633227770665c5831ca60
Anders Ijäs <andersjonat@gmail.com>
2026-05-01 11:59:36 +0200

    MCV layer created
    Controller created
    View created
    Printer created
    
    packages import to Main

--- src/main/java/se/kth/iv1350/bikeRepair/startup/Main.java
+++ src/main/java/se/kth/iv1350/bikeRepair/startup/Main.java
@@ -1,4 +1,9 @@
 package se.kth.iv1350.bikeRepair.startup;
 
+import se.kth.iv1350.bikeRepair.controller.Controller;
+import se.kth.iv1350.bikeRepair.view.View;
+import se.kth.iv1350.bikeRepair.integration.Printer;
 
-
+public class Main {
+    
+}
