a1f91ac48b4b718f20d261e5d48dc018608d15cb
Anders Ijäs <andersjonat@gmail.com>
2026-05-13 22:19:50 +0200

    Added missing javadoc comments

--- src/main/java/se/kth/iv1350/bikerepair/model/discountstrategy/BikeShopBirthdayDiscount.java
+++ src/main/java/se/kth/iv1350/bikerepair/model/discountstrategy/BikeShopBirthdayDiscount.java
@@ -4,6 +4,11 @@ import se.kth.iv1350.bikerepair.model.Amount;
 
 public class BikeShopBirthdayDiscount implements DiscountStrategy{
 
+    /**
+     * Calculates total amount based on the bike shops birthday discount.
+     * @param total The amount before discount
+     * @return Amount
+     */
     @Override
     public Amount calculateDiscount(Amount total) {
         return total.add(new Amount(-200));
--- src/main/java/se/kth/iv1350/bikerepair/model/discountstrategy/NoDiscount.java
+++ src/main/java/se/kth/iv1350/bikerepair/model/discountstrategy/NoDiscount.java
@@ -5,9 +5,6 @@ import se.kth.iv1350.bikerepair.model.Amount;
 public class NoDiscount implements DiscountStrategy {
 
     @Override
-    /**
-     * Calculates total discount in the case of no discount
-     */
     public Amount calculateDiscount(Amount total) {
         return total;
     }
