5b32609159429893b04f23e019767b6622bea95b
Anders Ijäs <andersjonat@gmail.com>
2026-05-14 12:19:03 +0200

    Update DataBaseFailureException.java

--- src/main/java/se/kth/iv1350/bikerepair/integration/DataBaseFailureException.java
+++ src/main/java/se/kth/iv1350/bikerepair/integration/DataBaseFailureException.java
@@ -5,6 +5,10 @@ package se.kth.iv1350.bikerepair.integration;
  */
 public class DataBaseFailureException extends Exception {
 
+    /**
+     * Exception for failure to connect to database. Hardcoded as of now.
+     * @param message The message to be logged
+     */
     public DataBaseFailureException(String message) {
         super(message);
     }
